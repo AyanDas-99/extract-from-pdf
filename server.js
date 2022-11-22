2e2a5e91c37764fabc6bbb763f58d7b698f1f77e
Ayan Das <ayandas9531@gmail.com>
2022-11-22 13:16:32 +0530

    sends file for extraction and saves in text file

--- server.js
+++ server.js
@@ -3,10 +3,29 @@ const app = express()
 const port = 3000
 const path = require('path')
 app.use(express.static('templates'))
+app.use('/static', express.static('public'))
+
+// multer
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' })
+
+// extract pdf
+const {storeToFile} = require('./extract');
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname, 'index.html')
 })
+
+
+app.post('/extract', upload.array('pdf', 1), async function (req, res, next) {
+  console.log(req.files)
+  fileName = await storeToFile(path.join(__dirname, req.files[0].path))
+  console.log(fileName)
+  res.redirect(`http://localhost:3000/static/`+fileName);
+  // res.sendFile(__dirname, '/static/'+d);
+})
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
