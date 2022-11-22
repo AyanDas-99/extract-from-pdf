f87912f7b67a81b53822ed91a00fb1547219ee8c
Ayan Das <ayandas9531@gmail.com>
2022-11-22 11:05:02 +0530

    tested extracting and saving to txt file

---
+++ extract.js
@@ -0,0 +1,39 @@
+// PDf extract
+const PDFExtract = require('pdf.js-extract').PDFExtract;
+const pdfExtract = new PDFExtract();
+const options = {}; /* see below */
+
+
+// write to text
+var fs = require('fs');
+
+
+pdfExtract.extract('test.pdf', options, (err, data) => {
+    if (err) return console.log(err);
+    //   console.log(data.pages[0].content);
+    loging(data, "nefile.txt")
+
+});
+
+function loging(data, fileName) {
+
+
+    var stream = fs.createWriteStream(fileName);
+    stream.once('open', function (fd) {
+
+
+        data.pages.forEach(page => {
+            page.content.forEach(element => {
+                // console.log(element.str);
+                stream.write(element.str+"\n");
+            })
+        });
+
+
+        stream.end();
+    });
+
+
+    saveStaticDataToFile(string);
+}
+
